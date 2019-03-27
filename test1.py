27d663572804d3e8e76e3d7afd66db3a41d7ebbd
lvyanwinder <48981125+lvyanwinder@users.noreply.github.com>
2019-03-27 17:33:06 +0800

    test1

--- test1.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-driver.find_element_by_id("kw").send_keys("selenium")
-driver.find_element_by_id("su").click()
-driver.quit()
